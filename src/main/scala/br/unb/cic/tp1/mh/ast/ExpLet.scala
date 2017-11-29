fe7389a2caf7998f49844501fdf32e232b4da8bd
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-29 16:51:56 -0200

    metodo que verifica tipo em ExpLet

--- src/main/scala/br/unb/cic/tp1/mh/ast/ExpLet.scala
+++ src/main/scala/br/unb/cic/tp1/mh/ast/ExpLet.scala
@@ -1,6 +1,6 @@
 package br.unb.cic.tp1.mh.ast
 
-import br.unb.cic.tp1.mh.memoria.Ambiente
+import br.unb.cic.tp1.mh.memoria._
 
 class ExpLet(val id : String, val expNomeada : Expressao, val corpo : Expressao) extends Expressao {
 
@@ -8,5 +8,9 @@ class ExpLet(val id : String, val expNomeada : Expressao, val corpo : Expressao)
     val valor = expNomeada.avaliar() // innermost strategy
     Ambiente.atualiza(id, valor)
     return corpo.avaliar()
+  }
+
+  override def verificaTipo(): Tipo = {
+    return corpo.verificaTipo()
   }
 }
